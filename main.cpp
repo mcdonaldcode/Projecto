8296b9e7c66a83d0afca64cbcf430878fd899d71
Jamie <59393679+JamieMcDonald43@users.noreply.github.com>
2020-01-18 17:06:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-cout << "Hello World" << endl;
+std::cout << "\n\tThis is the main menu of the game.\n" << endl
+  << "\nWill link to new, continue, saved, settings, quit, and others if applicable.\n" << endl;
+  return 0;
 }
